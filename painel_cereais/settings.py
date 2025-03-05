841352c316b6c2af05be937b8347ad0c6c1ef752
ribeiromelo <contatoeriquemelo@gmail.com>
2025-03-05 20:07:38 -0300

    Corrigindo PORT e Procfile

--- painel_cereais/settings.py
+++ painel_cereais/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-eid06a@nkv)e49^sruk7fdqon4jldoma%so*9hbb+!&uu*&e8-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
