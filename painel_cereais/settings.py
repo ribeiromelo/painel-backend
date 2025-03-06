485ee2dbf91d3ff0bba35d1da16e0a613a32b2d7
ribeiromelo <contatoeriquemelo@gmail.com>
2025-03-05 21:53:15 -0300

    Corrigindo erro de sintaxe no MIDDLEWARE

--- painel_cereais/settings.py
+++ painel_cereais/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',  #
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
 ]
+
 
 ROOT_URLCONF = 'painel_cereais.urls'
 
