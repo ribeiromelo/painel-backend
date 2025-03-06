0bdafb6539f78f33df1dacb1a25ba777d4500957
ribeiromelo <contatoeriquemelo@gmail.com>
2025-03-05 21:18:37 -0300

    Atualizando settings.py para conectar ao PostgreSQL

--- painel_cereais/settings.py
+++ painel_cereais/settings.py
@@ -88,20 +88,9 @@ WSGI_APPLICATION = 'painel_cereais.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")  # Obtém a URL do banco do Railway
-
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
-    }
-else:
-    # Caso não haja DATABASE_URL, isso será usado APENAS EM DEV (não produção)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
 
 
 # Password validation
