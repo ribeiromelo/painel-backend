5b54f7fccd600d281b5826b1869e2465db444890
ribeiromelo <contatoeriquemelo@gmail.com>
2025-03-05 23:28:33 -0300

    Correção CORS e CSRF para permitir frontend local

--- painel_cereais/settings.py
+++ painel_cereais/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'whitenoise.middleware.WhiteNoiseMiddleware',  #
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -67,7 +68,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 
@@ -173,4 +173,4 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 
-
+CORS_ALLOW_CREDENTIALS = True
